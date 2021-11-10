2dab021c5d1517dab0978bf778742b934be00bae
Nataf Cohen <nataf1231@gmail.com>
2021-11-10 10:07:18 +0200

    added backend

--- my-store/src/App.js
+++ my-store/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
+class App extends Component{
+constructor(props) 
+{
+  super(props);
+  this.state = { response: "" };
+}
+
+callAPI() 
+{
+  fetch("http://localhost:3000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+}
+
+componentWillMount() 
+{
+  this.callAPI();
+}
+
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p className="App-intro">;{this.state.apiResponse}</p>
     </div>
   );
 }
 
+}
 export default App;
